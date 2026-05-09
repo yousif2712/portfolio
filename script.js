081b3ba3f59a6058de5752f79db89650d1f1b134
Yousif Nazeer <yousifnazeer408@gmail.com>
2026-05-10 02:58:50 +0500

    Refactor smooth scroll navigation code

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+// Smooth scroll navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href"))
-            .scrollIntoView({
-                behavior: "smooth"
-            });
-    });
+anchor.addEventListener("click", function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+});
+});
 });
