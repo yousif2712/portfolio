fa39ca116eb8d883352dc1ad45060c73129e0913
Yousif Nazeer <yousifnazeer408@gmail.com>
2026-05-10 02:39:44 +0500

    Implement smooth scrolling for anchor links
    Added smooth scrolling for anchor links.

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-const faders=document.querySelectorAll(".fade");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
 
-window.addEventListener("scroll",()=>{
-  const trigger=window.innerHeight*0.85;
-  faders.forEach(section=>{
-    const top=section.getBoundingClientRect().top;
-    if(top<trigger){
-      section.classList.add("show");
-    }
-  });
-});
-
-const sections=document.querySelectorAll("section");
-const navLinks=document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll",()=>{
-  let current="";
-  sections.forEach(section=>{
-    const sectionTop=section.offsetTop-120;
-    if(scrollY>=sectionTop){
-      current=section.getAttribute("id");
-    }
-  });
-
-  navLinks.forEach(link=>{
-    link.classList.remove("active");
-    if(link.getAttribute("href")=="#"+current){
-      link.classList.add("active");
-    }
-  });
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+    });
 });
